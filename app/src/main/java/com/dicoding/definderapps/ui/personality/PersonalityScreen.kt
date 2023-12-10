1ada55dd5fb5124e820c16fa3cc527151b555247
whdadn <wahidadinugrohoo@gmail.com>
2023-12-10 22:47:30 +0800

    Membuat halaman personality

---
+++ app/src/main/java/com/dicoding/definderapps/ui/personality/PersonalityScreen.kt
@@ -0,0 +1,10 @@
+package com.dicoding.definderapps.ui.personality
+
+import androidx.compose.runtime.Composable
+import com.dicoding.definderapps.ui.component.personality.PersonalityItemPreview
+
+@Composable
+fun PersonalityScreen()
+{
+    PersonalityItemPreview()
+}
